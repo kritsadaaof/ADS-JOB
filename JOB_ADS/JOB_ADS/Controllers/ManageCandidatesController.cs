94c97536a8b26619fc7f7d787ed1f66eff1473e0
jirapatom <71012733+jirapatom@users.noreply.github.com>
2021-02-26 10:35:37 +0700

    26/02/2021
    Commit Select by Depart & Select by Position

--- JOB_ADS/JOB_ADS/Controllers/ManageCandidatesController.cs
+++ JOB_ADS/JOB_ADS/Controllers/ManageCandidatesController.cs
@@ -14,110 +14,85 @@ namespace JOB_ADS.Controllers
     {
         private Entities DbFile = new Entities();
         // GET: Manage Candidates
+        public class regis
+        {
+            public string position { get; set; }
+            public int number { get; set; }
+    
+        }
         public ActionResult Manage_Candidates()
         {
             if (Session["User"] == null)
             {
                 // check if a new session id was generated
-                return RedirectToAction("Index", "Home");
+                  return RedirectToAction("Index", "Home");
                 //return View();
             }
-            else
-            {
+            else { 
                 ViewBag.Titles = DbFile.ADS_Register.DistinctBy(x => x.Re_Position).ToList();
                 ViewBag.Departs = DbFile.ADS_Master_Department.OrderBy(a => a.Department_Name).ToList();
-                return View();
+                return View(); 
             }
 
         }
-        public ActionResult CandidatesDetail()
-        {
         
-            
-
-                return View();
-            
-
-        }
-        public ActionResult FindByDepart()
+        public ActionResult FindByDepart(string Depart)
         {
-            if (Session["User"] == null && Session["DEPARTMENT"] == null)
-            {
-                // check if a new session id was generated
-                return RedirectToAction("Index", "Home");
-                //   return View();
-            }
-            else 
-            {
-                string departnew = Session["DEPARTMENT"].ToString();
-                ViewBag.JobDepart = DbFile.ADS_Master_Department.Where(a => a.Department_Name.Contains(departnew)).Where(a => a.Status.Equals("T")).FirstOrDefault();
-                
-                return View();
-            }
-           
-        }
-      
-        [HttpPost]
-        public string DataFindByDepart(string Depart)
-        {
-            Session["DEPARTMENT"] = null;
+                 if (Session["User"] == null)
+                 {
+                     // check if a new session id was generated
+                     return RedirectToAction("Index", "Home");
+                     //   return View();
+                 }
+                 else
+                 { 
+            ViewBag.JobDepart = DbFile.ADS_Master_Department.Where(a => a.Department_Name.Contains(Depart)).Where(a => a.Status.Equals("T")).FirstOrDefault();
+                //ViewBag.JobRegis = DbFile.ADS_Register.DistinctBy(x => x.Re_Position).Where(a => a.Re_Department.Contains(Depart)).ToList();
             var data = (from x in DbFile.ADS_Register
-                        where x.Re_Department.Contains(Depart)
-                        group x by x.Re_Position into g
-                        select new
-                        {
-                            position = g.Key,
-                            number = g.Count()
-                        }).ToList();
-            if (data.Count != 0)
-            {
-                Session["DEPARTMENT"] = Depart;
-                string jsonlog = new JavaScriptSerializer().Serialize(data);
-                return jsonlog;
+                      where x.Re_Department.Contains(Depart)
+                      group x by x.Re_Position into g
+                      select new regis
+                      {
+                      position = g.Key,
+                      number = g.Count()
+                       }).ToList();
+
+
+                ViewBag.JobRegis = data;
+                return View();
             }
-            else
-            {
-                Session["DEPARTMENT"] = Depart;
-                return null;
-            }
-  
-        }
+         }
+       // public string DataFindByDepart(string Depart)
+        //{
+            
+            //ViewBag.JobDepart = DbFile.ADS_Master_Department.Where(a => a.Department_Name.Contains(Depart)).Where(a => a.Status.Equals("T")).FirstOrDefault();
+           // var data = (from x in DbFile.ADS_Register
+                      //  where x.Re_Department.Contains(Depart)
+                        //group x by x.Re_Position into g
+                        //select new
+                       // {
+                            //position = g.Key,
+                           // number = g.Count()
+                      //  }).ToList();
+                      
+            //string jsonlog = new JavaScriptSerializer().Serialize(data);
+            //return jsonlog;
+       // }
         public ActionResult FindByCandidates(string Position)
         {
-            if (Session["User"] == null)
-            {
-                // check if a new session id was generated
-                return RedirectToAction("Index", "Home");
-                //   return View();
-            }
-            else
-            {
+              if (Session["User"] == null)
+              {
+                  // check if a new session id was generated
+                    return RedirectToAction("Index", "Home");
+               //   return View();
+              }
+              else {
                 ViewBag.RegisName = DbFile.ADS_Register.Where(a => a.Re_Position.Contains(Position)).Where(a => a.Status.Equals("T")).FirstOrDefault();
                 ViewBag.RegisList = DbFile.ADS_Register.Where(a => a.Re_Position.Contains(Position))
                 .Where(a => a.Status.Equals("T")).ToList();
 
                 return View();
-            }
-
-        }
-        public string LoadDataDepart(string DEPART)
-        {
-            
-            var data = (from x in DbFile.ADS_Register
-                        where x.Re_Department.Equals(DEPART)
-                        group x by x.Re_Position into g
-                        select new
-                        {
-                            position = g.Key.ToString(),
-                            number = g.Count().ToString()
-                        }).AsEnumerable().Select(x => new
-                        {
-                            Position =x.position.ToString(),
-                            Number =x.number
-                        }
-                        ).ToList();
-            string jsonlog = new JavaScriptSerializer().Serialize(data);
-            return jsonlog;
+             }
 
         }
     }
